7b779d73ea145766006b00e2e8740a40e299e226
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2019-09-14 16:37:17 -0700

    not working yet; need crypto module

--- client/App.js
+++ client/App.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import Fortmatic from 'fortmatic'
+import Web3 from 'web3'
+
+const fm = new Fortmatic('YOUR_API_KEY')
+window.web3 = new Web3(fm.getProvider())
+
+import React, { useEffects } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
@@ -11,6 +17,12 @@ const styles = StyleSheet.create({
 })
 
 export const App = () => {
+  useEffects(() => {
+    fm.user.login().then(() => {
+      web3.eth.getAccounts().then(console.log) // ['0x...']
+    })
+  }, [])
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
